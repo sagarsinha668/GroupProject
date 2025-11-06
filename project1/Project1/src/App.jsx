ae725945501a58c98038d29a32e5e1134be5550b
sagarsinha668 <144223600+sagarsinha668@users.noreply.github.com>
2025-11-06 22:12:00 +0530

    app

---
+++ project1/Project1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState, useContext , useRef, useEffect } from "react"
+import Nav from "./components/nav"
+useState
+function App() {
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ project1/Project1/src/components/login.jsx
@@ -0,0 +1,9 @@
+
+export
+const Login = () => {
+    return (
+        <div>
+            <h2>Login Page</h2>
+        </div>
+    )
+}
